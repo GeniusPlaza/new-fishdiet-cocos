d33a009311e6a3e1305a236f89033d292f6f7a64
Denisse Lara <denisse@geniusplaza.com>
2017-09-25 14:34:32 -0400

    Add UI resources made in Cocos Studio

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 cc.game.onStart = function(){
     var sys = cc.sys;
     if(!sys.isNative && document.getElementById("cocosLoading")) //If referenced loading.js, please remove it
@@ -21,7 +20,7 @@ cc.game.onStart = function(){
     // cc.view.setOrientation(cc.ORIENTATION_PORTRAIT);
 
     // Setup the resolution policy and design resolution size
-    cc.view.setDesignResolutionSize(960, 600, cc.ResolutionPolicy.SHOW_ALL);
+    cc.view.setDesignResolutionSize(1920, 1080, cc.ResolutionPolicy.SHOW_ALL);
 
     // The game will be resized when browser size change
     cc.view.resizeWithBrowserSize(true);
--- src/resource.js
+++ src/resource.js
@@ -1,8 +1,9 @@
 var res = {
-    HelloWorld_png : "res/HelloWorld.png",
 };
 
 var g_resources = [];
 for (var i in res) {
     g_resources.push(res[i]);
 }
+
+# color F88C19FF
